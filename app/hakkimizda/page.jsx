c081d62eca8bb52f66700184f8a61db1337ce6cf
Kursat Keskin <kursatkeskinn@gmail.com>
2023-09-28 09:24:17 +0100

    About us image changed

--- app/hakkimizda/page.jsx
+++ app/hakkimizda/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Ozge2 from '../../assets/images/ozge.png'
+import logo1 from '@/assets/images/logo1.png'
 
 export default function Hakkimizda() {
   return (
@@ -15,8 +16,8 @@ export default function Hakkimizda() {
       <h1 className='text-2xl  border-b border-[#E5BA73] text-center mt-20'>Hakkımızda</h1>
        <div className='flex items-center justify-center'>
         <div className='flex mt-10 lg:mt-10 justify-center flex-wrap lg:flex-nowrap items-center text-center h-fit mb-20 w-[90%]'>
-         <div className='h-[400px] max-h-fit lg:w-2/5 w-full flex justify-center mx-5'>
-          <Image className='rounded-md' src={Ozge2} alt="" width={300} />
+         <div className='h-[150px] max-h-fit lg:w-2/5 w-full flex justify-center mx-5'>
+          <Image className='rounded-md' src={logo1} alt="" width={400} />
          </div>
             <div className='mt-10 md:mt-0 lg:mt-0 h-full lg:w-3/5 w-full lg:text-left text-center flex items-center justify-center'>
               <p>Özdemir Hukuk Ofisi 2023 yılında İstanbul merkezli olarak kurulmuştur. Hukuk ofisimizde başta
